89dd7064aaf30ac5a6deacccf3f68be582a45535
Aditya Atluri <adityaatluri@users.noreply.github.com>
2023-10-19 20:55:16 -0700

    Update and rename install-rock.sh to install-pre.sh

--- install-pre.sh
+++ install-pre.sh
@@ -1,15 +1,19 @@
 
 ## starts here!
+mkdir -p rxVega64fe
+cd rxVega64fe
+mkdir -p pre
+
 sudo apt update; sudo apt upgrade -y;
 sudo apt install make cmake build-essential -y
 sudo apt install libncurses5-dev flex bison libssl-dev libelf-dev -y
-mkdir -p rxVega64fe
-cd rxVega64fe
+
 git clone https://github.com/xgpu/ROCK-Kernel-Driver.git -b roc-2.0.x
 cd ROCK-Kernel-Driver
 make rock-rel_defconfig
 make -j `nproc`; make modules -j `nproc`; make bindeb-pkg LOCALVERSION=-rxvega64fe -j `nproc`
 cd ..
+
 sudo dpkg -i linux-headers-4.18.0-kfd-rxvega64fe_4.18.0-kfd-rxvega64fe-1_amd64.deb \
 linux-libc-dev_4.18.0-kfd-rxvega64fe-1_amd64.deb \
 linux-image-4.18.0-kfd-rxvega64fe_4.18.0-kfd-rxvega64fe-1_amd64.deb \
